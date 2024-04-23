ee6eb495860b7efd8da319aadfd3e1f91796b696
valeriana <claudiaae2@gmail.com>
2024-04-22 18:48:01 -0600

    SumaPOO

---
+++ Mainsuma.java
@@ -0,0 +1,8 @@
+public class MainSuma {
+    public static void main(String[]args) {
+        Suma n1 = new Suma();
+        n1.a= 3;
+        n1.b= 4;
+        System.out.println(n1.sumar());
+    }
+}
---
+++ Suma.java
@@ -0,0 +1,11 @@
+
+public class  Suma {
+int a, b, r;
+
+
+   
+public int sumar() {
+   r= this.a + this.b;
+   return r;
+}
+}
